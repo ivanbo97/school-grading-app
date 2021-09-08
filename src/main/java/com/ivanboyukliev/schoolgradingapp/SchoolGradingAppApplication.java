e7e215f803eceacf049b1efaf7d01388bc29bc46
ivanbo97 <ivanboukliev@abv.bg>
2021-09-08 16:02:51 +0300

    Initial commit

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/SchoolGradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ivanboyukliev.schoolgradingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolGradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolGradingAppApplication.class, args);
+	}
+
+}
