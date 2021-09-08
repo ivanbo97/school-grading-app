e896d2c811833af8500d20d1508685e5a1ec9529
ivanbo97 <ivanboukliev@abv.bg>
2021-09-08 18:15:42 +0300

    Class for storing app constants.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/util/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.ivanboyukliev.schoolgradingapp.util;
+
+public final class ApplicationConstants {
+	public static final String ENTITY_STUDENT_TABLE_NAME = "student";
+	public static final String ENTITY_STUDENT_ID_COLUMN = "student_id";
+	public static final String ENTITY_STUDENT_NAME_COLUMN = "student_name";
+
+	public static final String ENTITY_MARK_TABLE_NAME = "mark";
+	public static final String ENTITY_MARK_ID_COLUMN = "mark_id";
+	public static final String ENTITY_MARK_VAL_COLUMN = "mark";
+	public static final String ENTITY_MARK_DATE_COLUMN = "mark_date";
+
+}
