1be2c548c0fc4b3a9856283e32b5e807cf9e6d2e
ivanbo97 <ivanboukliev@abv.bg>
2021-09-08 18:14:59 +0300

    Entity class representing student table.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/domain/Student.java
@@ -0,0 +1,33 @@
+package com.ivanboyukliev.schoolgradingapp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = ENTITY_STUDENT_TABLE_NAME)
+public class Student {
+
+	@Id
+	@Column(name = ENTITY_STUDENT_ID_COLUMN)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = ENTITY_STUDENT_NAME_COLUMN)
+	private String studentName;
+
+}
