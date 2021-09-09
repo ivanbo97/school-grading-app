cd0c4c29db050ad9fba4b473af4886c0e61597d1
ivanbo97 <ivanboukliev@abv.bg>
2021-09-09 09:52:09 +0300

    Class for storing deserialized data corresponding to a row from a csv file.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/bootstrap/AggregatedDataBean.java
@@ -0,0 +1,31 @@
+package com.ivanboyukliev.schoolgradingapp.bootstrap;
+
+import org.springframework.stereotype.Component;
+
+import com.ivanboyukliev.schoolgradingapp.domain.Course;
+import com.ivanboyukliev.schoolgradingapp.domain.Mark;
+import com.ivanboyukliev.schoolgradingapp.domain.Student;
+import com.opencsv.bean.CsvRecurse;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AggregatedDataBean {
+
+	@CsvRecurse
+	private Student student;
+
+	@CsvRecurse
+	private Course course;
+
+	@CsvRecurse
+	private Mark mark;
+	
+	public void bindMarkToStudentAndCourse() {
+		
+		mark.setStudent(student);
+		mark.setCourse(course);
+	}
+}
