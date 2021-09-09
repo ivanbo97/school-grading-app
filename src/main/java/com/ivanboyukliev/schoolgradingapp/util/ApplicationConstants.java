af3127569d72140c97a6a230701573e65550f095
ivanbo97 <ivanboukliev@abv.bg>
2021-09-09 16:04:18 +0300

    Added constants for DTO properties.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/util/ApplicationConstants.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/util/ApplicationConstants.java
@@ -11,22 +11,25 @@ public final class ApplicationConstants {
 	public static final String ENTITY_MARK_DATE_COLUMN = "mark_date";
 	public static final String ENTITY_MARK_STUDENT_COLUMN = "id_student";
 	public static final String ENTITY_MARK_COURSE_COLUMN = "id_course";
-    public static final String ENTITY_MARK_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
-	
+	public static final String ENTITY_MARK_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+
 	public static final String ENTITY_COURSE_TABLE_NAME = "course";
 	public static final String ENTITY_COURSE_ID_COLUMN = "course_id";
 	public static final String ENTITY_COURSE_NAME_COLUMN = "course_name";
-	
-    //== CSV header names constants ==
-    public static final String CSV_HEADER_COURSE_ID = "course_id";
-    public static final String CSV_HEADER_COURSE_NAME = "course_name";
 
-    public static final String CSV_HEADER_STUDENT_ID = "student_id";
-    public static final String CSV_HEADER_STUDENT_NAME = "student_name";
+	// == CSV header names constants ==
+	public static final String CSV_HEADER_COURSE_ID = "course_id";
+	public static final String CSV_HEADER_COURSE_NAME = "course_name";
 
-    public static final String CSV_HEADER_MARK_ID = "mark_id";
-    public static final String CSV_HEADER_MARK = "mark";
-    public static final String CSV_HEADER_MARK_DATE = "mark_date";
+	public static final String CSV_HEADER_STUDENT_ID = "student_id";
+	public static final String CSV_HEADER_STUDENT_NAME = "student_name";
 
+	public static final String CSV_HEADER_MARK_ID = "mark_id";
+	public static final String CSV_HEADER_MARK = "mark";
+	public static final String CSV_HEADER_MARK_DATE = "mark_date";
+
+	// End Point Mappings
+	public static final String API_V1 = "/api/v1";
+	public static final String STUDENT_BASE_URL = API_V1 + "/student";
 
 }
