74ee08a2b5d8d0a302ed15851e7dac6a30d709b5
ivanbo97 <ivanboukliev@abv.bg>
2021-09-09 06:57:34 +0300

    Added new constants for new entities.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/util/ApplicationConstants.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/util/ApplicationConstants.java
@@ -9,5 +9,11 @@ public final class ApplicationConstants {
 	public static final String ENTITY_MARK_ID_COLUMN = "mark_id";
 	public static final String ENTITY_MARK_VAL_COLUMN = "mark";
 	public static final String ENTITY_MARK_DATE_COLUMN = "mark_date";
+	public static final String ENTITY_MARK_STUDENT_COLUMN = "id_student";
+	public static final String ENTITY_MARK_COURSE_COLUMN = "id_course";
+	
+	public static final String ENTITY_COURSE_TABLE_NAME = "course";
+	public static final String ENTITY_COURSE_ID_COLUMN = "course_id";
+	public static final String ENTITY_COURSE_NAME_COLUMN = "course_name";
 
 }
