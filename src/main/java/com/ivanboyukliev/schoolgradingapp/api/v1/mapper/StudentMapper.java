306c1ef61cbc28624366a0e9153cd6e854b89f8c
ivanbo97 <ivanboukliev@abv.bg>
2021-09-09 16:05:37 +0300

    Mapper interface for transforming Student to StudentDTO and vice versa.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/api/v1/mapper/StudentMapper.java
@@ -0,0 +1,27 @@
+package com.ivanboyukliev.schoolgradingapp.api.v1.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.StudentDTO;
+import com.ivanboyukliev.schoolgradingapp.domain.Student;
+
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.*;
+
+@Mapper
+public interface StudentMapper {
+	
+	StudentMapper INSTANCE = Mappers.getMapper(StudentMapper.class);
+	
+	default StudentDTO studentToStudentDTO(Student student) {
+		return StudentDTO.builder()
+				.name(student.getStudentName())
+				.studentUrl(STUDENT_BASE_URL + "/" + student.getId())
+				.build();
+	}
+	
+	@Mapping(source = "name", target = "name")
+	Student studentDTOToStudent(StudentDTO studentDTO);
+
+}
