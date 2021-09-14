4e5a5a9783c19096911ae1d9c6ef2cd5c01a92b9
ivanbo97 <ivanboukliev@abv.bg>
2021-09-14 15:38:29 +0300

    Implemented a service for getting student  by id.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/service/StudentServiceImpl.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/service/StudentServiceImpl.java
@@ -1,12 +1,16 @@
 package com.ivanboyukliev.schoolgradingapp.service;
 
 import com.ivanboyukliev.schoolgradingapp.api.v1.mapper.StudentMapper;
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.StudentDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.StudentListDTO;
+import com.ivanboyukliev.schoolgradingapp.exception.EntityNotFoundCustomException;
 import com.ivanboyukliev.schoolgradingapp.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
+
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.ERROR_STUDENT_NOT_FOUND;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -27,7 +31,15 @@ public class StudentServiceImpl implements StudentService {
                 this.studentRepository.findAll()
                         .stream()
                         .map(studentMapper::studentToStudentDTO)
-                        .collect(Collectors.toList())
-        );
+                        .collect(Collectors.toList()));
+
+    }
+
+    @Override
+    public StudentDTO findStudentById(Long id) {
+        return this.studentRepository.findById(id)
+                .map(studentMapper::studentToStudentDTO)
+                .orElseThrow(() -> new EntityNotFoundCustomException(
+                        String.format(ERROR_STUDENT_NOT_FOUND, id)));
     }
 }
