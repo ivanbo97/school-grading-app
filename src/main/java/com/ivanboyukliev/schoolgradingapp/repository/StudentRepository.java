fbaf10f741938ee9b6fa3fcea9a0ad27d6f97193
ivanbo97 <ivanboukliev@abv.bg>
2021-09-15 17:24:22 +0300

    Added method for getting student by name.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/repository/StudentRepository.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/repository/StudentRepository.java
@@ -1,11 +1,15 @@
 package com.ivanboyukliev.schoolgradingapp.repository;
 
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.StudentDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ivanboyukliev.schoolgradingapp.domain.Student;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    Optional<Student> findStudentByName(String studentName);
 }
