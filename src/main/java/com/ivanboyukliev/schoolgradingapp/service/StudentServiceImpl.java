f7c446e5e7de5883644586ac1b20ad540f34fd2f
ivanbo97 <ivanboukliev@abv.bg>
2021-09-15 17:26:02 +0300

    Added method for saving student in repository.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/service/StudentServiceImpl.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/service/StudentServiceImpl.java
@@ -3,13 +3,18 @@ package com.ivanboyukliev.schoolgradingapp.service;
 import com.ivanboyukliev.schoolgradingapp.api.v1.mapper.StudentMapper;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.StudentDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.StudentListDTO;
+import com.ivanboyukliev.schoolgradingapp.domain.Student;
 import com.ivanboyukliev.schoolgradingapp.exception.EntityNotFoundCustomException;
+import com.ivanboyukliev.schoolgradingapp.exception.EntityValidationException;
 import com.ivanboyukliev.schoolgradingapp.repository.StudentRepository;
+import com.ivanboyukliev.schoolgradingapp.validation.BaseNamedEntityValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.ERROR_STUDENT_EXISTS;
 import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.ERROR_STUDENT_NOT_FOUND;
 
 @Service
@@ -17,12 +22,15 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
+    private final BaseNamedEntityValidator basedNamedEntityValidator;
 
     @Autowired
     public StudentServiceImpl(StudentRepository studentRepository,
-                              StudentMapper studentMapper) {
+                              StudentMapper studentMapper,
+                              BaseNamedEntityValidator entityValidator) {
         this.studentRepository = studentRepository;
         this.studentMapper = studentMapper;
+        this.basedNamedEntityValidator = entityValidator;
     }
 
     @Override
@@ -41,5 +49,25 @@ public class StudentServiceImpl implements StudentService {
                 .map(studentMapper::studentToStudentDTO)
                 .orElseThrow(() -> new EntityNotFoundCustomException(
                         String.format(ERROR_STUDENT_NOT_FOUND, id)));
+    }
+
+    @Override
+    public StudentDTO saveStudent(StudentDTO student) throws EntityValidationException {
+        this.basedNamedEntityValidator.validate(student);
+        this.validateStudentExistence(student);
+        return this.saveStudentToDatabase(studentMapper.studentDTOToStudent(student));
+    }
+
+    private StudentDTO saveStudentToDatabase(Student student) {
+        Student savedStudent = this.studentRepository.save(student);
+        System.out.println("*******SAVED STUDENT" + savedStudent.getId());
+        return this.studentMapper.studentToStudentDTO(savedStudent);
+    }
+
+    private void validateStudentExistence(StudentDTO student) throws EntityValidationException {
+        Optional<Student> searchedStudent = this.studentRepository.findStudentByName(student.getName());
+        if (searchedStudent.isPresent()) {
+            throw new EntityValidationException(String.format(ERROR_STUDENT_EXISTS, student.getName()));
+        }
     }
 }
