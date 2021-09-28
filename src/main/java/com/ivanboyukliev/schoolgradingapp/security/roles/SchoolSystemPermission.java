e5a4f543c9fb7e8c2c8768496d3ffd7599191c28
ivanbo97 <ivanboukliev@abv.bg>
2021-09-28 09:48:15 +0300

    Added user roles for interaction with the school grading system.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/roles/SchoolSystemPermission.java
@@ -0,0 +1,22 @@
+package com.ivanboyukliev.schoolgradingapp.security.roles;
+
+public enum SchoolSystemPermission {
+
+    MARK_READ("mark:read"),
+    MARK_WRITE("mark-write"),
+    STUDENT_READ("student-read"),
+    STUDENT_WRITE("student-write"),
+    COURSE_READ("course-read"),
+    COURSE_WRITE("course-write"),
+    AVERAGE_MARK_READ("avg-read");
+
+    private final String permission;
+
+    SchoolSystemPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
