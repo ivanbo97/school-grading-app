f48255c3f323b95f99f4fe79afb73f7d99aac9fa
ivanbo97 <ivanboukliev@abv.bg>
2021-09-28 13:19:40 +0300

    Adding data to credentials table.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/bootstrap/DataLoader.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/bootstrap/DataLoader.java
@@ -9,8 +9,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.ivanboyukliev.schoolgradingapp.domain.SchoolSystemCredential;
+import com.ivanboyukliev.schoolgradingapp.security.roles.SchoolSystemUserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.ivanboyukliev.schoolgradingapp.domain.Course;
@@ -24,65 +27,91 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
+import static com.ivanboyukliev.schoolgradingapp.security.roles.SchoolSystemUserRole.ADMIN;
+import static com.ivanboyukliev.schoolgradingapp.security.roles.SchoolSystemUserRole.USER;
+
 @Slf4j
 @Component
 public class DataLoader implements CommandLineRunner {
 
-	private final StudentRepository studentRepository;
-	private final CourseRepository courseRepository;
-	private final MarkRepository markRepository;
+    private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+    private final MarkRepository markRepository;
 
-	private final Set<Student> retrievedStudents;
-	private final Set<Course> retrievedCourses;
-	private final Set<Mark> retrievedMarks;
+    private final Set<Student> retrievedStudents;
+    private final Set<Course> retrievedCourses;
+    private final Set<Mark> retrievedMarks;
 
-	@Autowired
-	public DataLoader(StudentRepository studentRepository, CourseRepository courseRepository,
-			MarkRepository markRepository) {
-		this.studentRepository = studentRepository;
-		this.courseRepository = courseRepository;
-		this.markRepository = markRepository;
-		retrievedStudents = new HashSet<Student>();
-		retrievedMarks = new HashSet<Mark>();
-		retrievedCourses = new HashSet<Course>();
-	}
+    private final PasswordEncoder passwordEncoder;
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Autowired
+    public DataLoader(StudentRepository studentRepository,
+                      CourseRepository courseRepository,
+                      MarkRepository markRepository,
+                      PasswordEncoder passwordEncoder) {
 
-		List<AggregatedDataBean> allAggregatedData = loadDataFromFile();
-		processRetrievedData(allAggregatedData);
+        this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
+        this.markRepository = markRepository;
+        this.passwordEncoder = passwordEncoder;
+        retrievedStudents = new HashSet<Student>();
+        retrievedMarks = new HashSet<Mark>();
+        retrievedCourses = new HashSet<Course>();
+    }
 
-		studentRepository.saveAll(retrievedStudents);
-		courseRepository.saveAll(retrievedCourses);
-		markRepository.saveAll(retrievedMarks);
+    @Override
+    public void run(String... args) throws Exception {
 
-		log.info("Sucessful saved data to DB.");
-	}
+        List<AggregatedDataBean> allAggregatedData = loadDataFromFile();
+        processRetrievedData(allAggregatedData);
 
-	private List<AggregatedDataBean> loadDataFromFile() {
+        studentRepository.saveAll(retrievedStudents);
+        courseRepository.saveAll(retrievedCourses);
+        markRepository.saveAll(retrievedMarks);
 
-		List<AggregatedDataBean> allAggregatedData = new ArrayList<>();
+        log.info("Adding credentials...");
+        for (long i = 0; i < 5; i++) {
+            SchoolSystemUserRole role = (i % 2 != 0) ? USER : ADMIN;
+            Student student = studentRepository.findById(i).orElseThrow();
+            addCredentials(student, role);
+        }
+        log.info("Sucessful saved data to DB.");
+    }
 
-		try (InputStream inputStream = getClass().getResourceAsStream("/marks.csv")) {
-			allAggregatedData = new CsvToBeanBuilder<AggregatedDataBean>(
-					new CSVReader(new BufferedReader(new InputStreamReader(inputStream))))
-							.withType(AggregatedDataBean.class).build().parse();
-		} catch (IOException exc) {
-			log.info("Problem occured during file opening: " + exc.getMessage());
-		}
-		log.info("Data loading completed successfully!");
-		return allAggregatedData;
-	}
+    private List<AggregatedDataBean> loadDataFromFile() {
 
-	private void processRetrievedData(List<AggregatedDataBean> aggregatedDataBeans) {
+        List<AggregatedDataBean> allAggregatedData = new ArrayList<>();
 
-		for (AggregatedDataBean dataBean : aggregatedDataBeans) {
-			dataBean.bindMarkToStudentAndCourse();
-			retrievedStudents.add(dataBean.getStudent());
-			retrievedCourses.add(dataBean.getCourse());
-			retrievedMarks.add(dataBean.getMark());
-		}
-	}
+        try (InputStream inputStream = getClass().getResourceAsStream("/marks.csv")) {
+            allAggregatedData = new CsvToBeanBuilder<AggregatedDataBean>(
+                    new CSVReader(new BufferedReader(new InputStreamReader(inputStream))))
+                    .withType(AggregatedDataBean.class).build().parse();
+        } catch (IOException exc) {
+            log.info("Problem occured during file opening: " + exc.getMessage());
+        }
+        log.info("Data loading completed successfully!");
+        return allAggregatedData;
+    }
+
+    private void processRetrievedData(List<AggregatedDataBean> aggregatedDataBeans) {
+
+        for (AggregatedDataBean dataBean : aggregatedDataBeans) {
+            dataBean.bindMarkToStudentAndCourse();
+            retrievedStudents.add(dataBean.getStudent());
+            retrievedCourses.add(dataBean.getCourse());
+            retrievedMarks.add(dataBean.getMark());
+        }
+    }
+
+    private void addCredentials(Student student, SchoolSystemUserRole role) {
+        SchoolSystemCredential credential = SchoolSystemCredential.builder()
+                .username(student.getName() + student.getId())
+                .password(passwordEncoder.encode("password"))
+                .student(student)
+                .userRole(role)
+                .build();
+        student.setCredential(credential);
+        studentRepository.save(student);
+    }
 
 }
