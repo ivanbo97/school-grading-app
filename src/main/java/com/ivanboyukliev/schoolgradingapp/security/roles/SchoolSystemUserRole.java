575ceb8747f5d5904d3aaec373ed39fe3000128d
ivanbo97 <ivanboukliev@abv.bg>
2021-09-28 10:07:18 +0300

    Enum that corresponds to two basic roles in the system: user and admin.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/roles/SchoolSystemUserRole.java
@@ -0,0 +1,38 @@
+package com.ivanboyukliev.schoolgradingapp.security.roles;
+
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.ivanboyukliev.schoolgradingapp.security.roles.SchoolSystemPermission.*;
+
+public enum SchoolSystemUserRole {
+    USER(Sets.newHashSet(AVERAGE_MARK_READ)),
+    ADMIN(Sets.newHashSet(
+            MARK_READ, MARK_WRITE,
+            STUDENT_READ, STUDENT_WRITE,
+            COURSE_READ, COURSE_WRITE,
+            AVERAGE_MARK_READ
+    ));
+
+    private final Set<SchoolSystemPermission> permissions;
+
+    SchoolSystemUserRole(Set<SchoolSystemPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<SchoolSystemPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
+}
