fe4f5d27cb4da846cdadb013e77bd4a0a3581f28
ivanbo97 <ivanboukliev@abv.bg>
2021-10-04 09:54:42 +0300

    Added test for assuring the correctness of method related to getting all courses.

---
+++ src/test/java/com/ivanboyukliev/schoolgradingapp/service/CourseServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.ivanboyukliev.schoolgradingapp.service;
+
+import com.ivanboyukliev.schoolgradingapp.api.v1.mapper.CourseMapper;
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.CourseListDTO;
+import com.ivanboyukliev.schoolgradingapp.domain.Course;
+import com.ivanboyukliev.schoolgradingapp.repository.CourseRepository;
+import com.ivanboyukliev.schoolgradingapp.validation.BaseNamedEntityValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CourseServiceImplTest {
+
+
+    private CourseServiceImpl courseService;
+
+    @Mock
+    private CourseRepository courseRepository;
+
+    @Mock
+    private BaseNamedEntityValidator entityValidator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        courseService = new CourseServiceImpl(courseRepository, CourseMapper.INSTANCE, entityValidator);
+    }
+
+    @Test
+    void findAllCourses() {
+        // given
+        Course mathCourse = new Course(1L, "Math");
+        Course biologyCourse = new Course(2L, "Biology");
+
+        List<Course> courses = new ArrayList<>();
+        courses.add(mathCourse);
+        courses.add(biologyCourse);
+
+        given(courseRepository.findAll())
+                .willReturn(courses);
+
+        // when
+        CourseListDTO retrievedCourses = courseService.findAllCourses();
+
+        // then
+        assertEquals(2, retrievedCourses.getCourses().size());
+    }
+}
