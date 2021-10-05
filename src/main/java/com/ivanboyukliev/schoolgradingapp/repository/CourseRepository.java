8ebfb9ca713743dfb8dae4dfd91a66f7892370de
ivanbo97 <ivanboukliev@abv.bg>
2021-10-05 14:21:30 +0300

    Added new method for getting course by its name.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/repository/CourseRepository.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/repository/CourseRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ivanboyukliev.schoolgradingapp.domain.Course;
 
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    Optional<Course> findCourseByName(String courseName);
 }
