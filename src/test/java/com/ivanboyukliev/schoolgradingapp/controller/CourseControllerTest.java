7824e69b820230277f0998b59b16e7374c88b544
ivanbo97 <ivanboukliev@abv.bg>
2021-10-08 16:12:45 +0300

    Added test for controller response to get course by id method.

--- src/test/java/com/ivanboyukliev/schoolgradingapp/controller/CourseControllerTest.java
+++ src/test/java/com/ivanboyukliev/schoolgradingapp/controller/CourseControllerTest.java
@@ -14,7 +14,9 @@ import java.util.Arrays;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,5 +46,19 @@ class CourseControllerTest {
                 .andExpect(jsonPath("$.courses[0].course_name", is(mathCourse.getName())));
 
 
+    }
+
+    @Test
+    void findCourseByIdTest() throws Exception {
+        // given
+        CourseDTO biologyCourse = new CourseDTO("Biology", "api/ve/course/7");
+        given(courseService.findCourseById(anyLong())).willReturn(biologyCourse);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/course/7")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.course_name", is(biologyCourse.getName())));
+        then(courseService).should().findCourseById(7L);
     }
 }
