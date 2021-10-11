fee9535f2457019405445429518d73b3db48c066
ivanbo97 <ivanboukliev@abv.bg>
2021-10-11 19:31:53 +0300

    Added end-point for post request.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/controller/MarkController.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/controller/MarkController.java
@@ -1,13 +1,12 @@
 package com.ivanboyukliev.schoolgradingapp.controller;
 
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkListDTO;
+import com.ivanboyukliev.schoolgradingapp.exception.EntityValidationException;
 import com.ivanboyukliev.schoolgradingapp.service.MarkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.MARK_BASE_URL;
 
@@ -26,5 +25,11 @@ public class MarkController {
     @ResponseStatus(HttpStatus.OK)
     MarkListDTO findAllCourses() {
         return markService.findAllMarks();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    MarkDTO saveMark(@RequestBody MarkDTO markDTO) throws EntityValidationException {
+        return markService.saveMark(markDTO);
     }
 }
