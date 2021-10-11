a075c690e32085a060fde214a303d37cd60823aa
ivanbo97 <ivanboukliev@abv.bg>
2021-10-11 18:35:56 +0300

    Added test method for getting mark by its id functionality.

--- src/test/java/com/ivanboyukliev/schoolgradingapp/service/MarkServiceImplTest.java
+++ src/test/java/com/ivanboyukliev/schoolgradingapp/service/MarkServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.ivanboyukliev.schoolgradingapp.service;
 
 import com.ivanboyukliev.schoolgradingapp.api.v1.mapper.MarkMapper;
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkListDTO;
 import com.ivanboyukliev.schoolgradingapp.domain.Course;
 import com.ivanboyukliev.schoolgradingapp.domain.Mark;
@@ -15,12 +16,17 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.ENTITY_MARK_DATE_TIME_FORMAT;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class MarkServiceImplTest {
@@ -56,5 +62,29 @@ class MarkServiceImplTest {
 
         // then
         assertEquals(3, retrievedMarks.getMarks().size());
+    }
+
+    @Test
+    void findMarkByIdTest() {
+        // given
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(ENTITY_MARK_DATE_TIME_FORMAT);
+        Course course = new Course(1L, "TestCourseOne");
+        Student student = new Student(1L, "TestStudentOne", new HashSet<>());
+        Mark retrievedMark = new Mark(13L, 5.00d, LocalDateTime.now(), student, course);
+
+        given(markRepository.findById(anyLong())).willReturn(Optional.of(retrievedMark));
+
+        // when
+        MarkDTO foundMark = markService.findMarkById(13L);
+
+        // then
+        assertAll("Perform multiple assumptions on MarkDTO",
+                () -> assertNotNull(foundMark),
+                () -> assertEquals(course.getName(), foundMark.getCourseName()),
+                () -> assertEquals(student.getName(), foundMark.getStudentName()),
+                () -> assertEquals(retrievedMark.getMark(), foundMark.getMark()),
+                () -> assertEquals(retrievedMark.getMarkDate().format(dtf), foundMark.getDate())
+        );
+        then(markRepository).should().findById(13L);
     }
 }
