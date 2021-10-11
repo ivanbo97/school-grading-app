329ff50750708dfa59b23aff708214ed954b92a5
ivanbo97 <ivanboukliev@abv.bg>
2021-10-11 17:42:13 +0300

    Implementing functionality for getting avg mark of a student for particular course.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/service/ReportServiceImpl.java
@@ -0,0 +1,79 @@
+package com.ivanboyukliev.schoolgradingapp.service;
+
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.report.ReportDTO;
+import com.ivanboyukliev.schoolgradingapp.exception.EntityValidationException;
+import com.ivanboyukliev.schoolgradingapp.repository.CourseRepository;
+import com.ivanboyukliev.schoolgradingapp.repository.MarkRepository;
+import com.ivanboyukliev.schoolgradingapp.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.ERROR_COURSE_NOT_FOUND;
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.ERROR_STUDENT_NOT_FOUND;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+
+    MarkRepository markRepository;
+    CourseRepository courseRepository;
+    StudentRepository studentRepository;
+
+    @Autowired
+    public ReportServiceImpl(MarkRepository markRepository, CourseRepository courseRepository, StudentRepository studentRepository) {
+        this.markRepository = markRepository;
+        this.courseRepository = courseRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public ReportDTO avgStudentMarkForCourse(Long studentId, Long courseId) throws EntityValidationException {
+
+        this.validateCourseExistence(courseId);
+        this.validateStudentExistence(studentId);
+        Double avgMark = markRepository.avgMarkForAStudentInACourse(studentId, courseId);
+        return formatReport(avgMark);
+    }
+
+    @Override
+    public ReportDTO avgStudentMarkAllCourses(Long studentId) throws EntityValidationException {
+        return null;
+    }
+
+    @Override
+    public ReportDTO avgMarkForACourse(Long courseId) throws EntityValidationException {
+        return null;
+    }
+
+    @Override
+    public ReportDTO avgMarkAllStudentsForAllCourses() {
+        return null;
+    }
+
+    @Override
+    public ReportDTO avgMarkForCombinationsForStudentAndCourse() {
+        return null;
+    }
+
+    //== private methods ==
+    private void validateStudentExistence(Long studentId) throws EntityValidationException {
+        if (!this.studentRepository.existsById(studentId)) {
+            throw new EntityValidationException(String.format(ERROR_STUDENT_NOT_FOUND, studentId));
+        }
+    }
+
+    private void validateCourseExistence(Long courseId) throws EntityValidationException {
+        if (!this.courseRepository.existsById(courseId)) {
+            throw new EntityValidationException(String.format(ERROR_COURSE_NOT_FOUND, courseId));
+        }
+    }
+
+    private ReportDTO formatReport(Double result) {
+        log.info("Result = {}", result);
+        String formattedResult = result != null
+                ? String.format("Average Result = %.2f", result)
+                : "No Data Found";
+        return new ReportDTO(formattedResult);
+    }
+}
