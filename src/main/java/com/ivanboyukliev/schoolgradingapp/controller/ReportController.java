fd3f96606c36d51918082a818b8f2131016ffea0
ivanbo97 <ivanboukliev@abv.bg>
2021-10-11 17:43:00 +0300

    Exposing an end-point for getting the average mark of a student for a particular course.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/controller/ReportController.java
@@ -0,0 +1,30 @@
+package com.ivanboyukliev.schoolgradingapp.controller;
+
+import com.ivanboyukliev.schoolgradingapp.api.v1.model.report.ReportDTO;
+import com.ivanboyukliev.schoolgradingapp.exception.EntityValidationException;
+import com.ivanboyukliev.schoolgradingapp.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.REPORT_BASE_URL;
+
+@RestController
+@RequestMapping(REPORT_BASE_URL)
+public class ReportController {
+
+    private ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/avg/student/{studentId}/course/{courseId}")
+    @ResponseStatus(HttpStatus.OK)
+    ReportDTO getAvgMarkForStudentInCourse(@PathVariable String studentId,
+                                           @PathVariable String courseId) throws EntityValidationException {
+        return reportService.avgStudentMarkForCourse(
+                Long.valueOf(studentId), Long.valueOf(courseId));
+    }
+}
