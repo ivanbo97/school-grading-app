fbe1b67ea8bc7def4f2b3f12de872a82ab6b95dc
ivanbo97 <ivanboukliev@abv.bg>
2021-10-15 15:07:19 +0300

    Added test for DELETE controller end-point.

--- src/test/java/com/ivanboyukliev/schoolgradingapp/controller/MarkControllerTest.java
+++ src/test/java/com/ivanboyukliev/schoolgradingapp/controller/MarkControllerTest.java
@@ -2,7 +2,6 @@ package com.ivanboyukliev.schoolgradingapp.controller;
 
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkListDTO;
-import com.ivanboyukliev.schoolgradingapp.exception.EntityValidationException;
 import com.ivanboyukliev.schoolgradingapp.service.MarkService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +12,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
 
 import static com.ivanboyukliev.schoolgradingapp.controller.StudentControllerTest.asJsonString;
 import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.MARK_BASE_URL;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -104,10 +101,18 @@ class MarkControllerTest {
                         .content(asJsonString(markForUpdate)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.mark", is(markForUpdate.getMark())))
-                .andExpect(jsonPath("$.date",is(markForUpdate.getDate())))
-                .andExpect(jsonPath("$.mark_url",is(markForUpdate.getMarkUrl())))
+                .andExpect(jsonPath("$.date", is(markForUpdate.getDate())))
+                .andExpect(jsonPath("$.mark_url", is(markForUpdate.getMarkUrl())))
                 .andExpect(jsonPath("$.student_name", is(markForUpdate.getStudentName())))
                 .andExpect(jsonPath("$.course_name", is(markForUpdate.getCourseName())));
-        then(markService).should().updateMark(anyLong(),any(MarkDTO.class));
+        then(markService).should().updateMark(anyLong(), any(MarkDTO.class));
+    }
+
+    @Test
+    void deleteMarkTest() throws Exception {
+
+        mockMvc.perform(delete(MARK_BASE_URL + "/7")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
