b19c23577326848e3d5032e89a4bab596c65ab80
ivanbo97 <ivanboukliev@abv.bg>
2021-10-15 15:01:19 +0300

    Added test for PUT controller end-point.

--- src/test/java/com/ivanboyukliev/schoolgradingapp/controller/MarkControllerTest.java
+++ src/test/java/com/ivanboyukliev/schoolgradingapp/controller/MarkControllerTest.java
@@ -2,6 +2,7 @@ package com.ivanboyukliev.schoolgradingapp.controller;
 
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkListDTO;
+import com.ivanboyukliev.schoolgradingapp.exception.EntityValidationException;
 import com.ivanboyukliev.schoolgradingapp.service.MarkService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,12 +82,32 @@ class MarkControllerTest {
         // when, then
 
         mockMvc.perform(post(MARK_BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(providedMark)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(providedMark)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.mark",is(providedMark.getMark())))
-                .andExpect(jsonPath("$.student_name",is(providedMark.getStudentName())));
+                .andExpect(jsonPath("$.mark", is(providedMark.getMark())))
+                .andExpect(jsonPath("$.student_name", is(providedMark.getStudentName())));
 
         then(markService).should().saveMark(any());
+    }
+
+    @Test
+    void updateMarkTest() throws Exception {
+
+        // given
+        MarkDTO markForUpdate = new MarkDTO(4.5, LocalDateTime.now().toString(), "Georgi Doe", "Psychology", "api/v1/mark/7");
+        given(markService.updateMark(anyLong(), any(MarkDTO.class))).willReturn(markForUpdate);
+
+        // when, then
+        mockMvc.perform(put(MARK_BASE_URL + "/7")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(markForUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.mark", is(markForUpdate.getMark())))
+                .andExpect(jsonPath("$.date",is(markForUpdate.getDate())))
+                .andExpect(jsonPath("$.mark_url",is(markForUpdate.getMarkUrl())))
+                .andExpect(jsonPath("$.student_name", is(markForUpdate.getStudentName())))
+                .andExpect(jsonPath("$.course_name", is(markForUpdate.getCourseName())));
+        then(markService).should().updateMark(anyLong(),any(MarkDTO.class));
     }
 }
