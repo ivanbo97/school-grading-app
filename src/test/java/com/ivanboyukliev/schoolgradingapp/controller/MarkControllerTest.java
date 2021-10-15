52df419a642364d129fa9bf21569ee70d4d30db6
ivanbo97 <ivanboukliev@abv.bg>
2021-10-15 14:39:59 +0300

    Added test for findById and saveMark methods.

--- src/test/java/com/ivanboyukliev/schoolgradingapp/controller/MarkControllerTest.java
+++ src/test/java/com/ivanboyukliev/schoolgradingapp/controller/MarkControllerTest.java
@@ -2,6 +2,7 @@ package com.ivanboyukliev.schoolgradingapp.controller;
 
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkDTO;
 import com.ivanboyukliev.schoolgradingapp.api.v1.model.MarkListDTO;
+import com.ivanboyukliev.schoolgradingapp.service.MarkService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,11 +14,15 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.ivanboyukliev.schoolgradingapp.controller.StudentControllerTest.asJsonString;
 import static com.ivanboyukliev.schoolgradingapp.util.ApplicationConstants.MARK_BASE_URL;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MarkControllerTest {
 
     @MockBean
-    private MarkController markController;
+    private MarkService markService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,7 +43,7 @@ class MarkControllerTest {
         MarkDTO mark1 = new MarkDTO(5.5, LocalDateTime.now().toString(), "Ivan Doe", "History", "api/v1/mark/1");
         MarkDTO mark2 = new MarkDTO(4.5, LocalDateTime.now().toString(), "Ivan Doe", "Agriculture", "api/v1/mark/1");
         MarkListDTO retrievedMarks = new MarkListDTO(Arrays.asList(mark1, mark2));
-        given(markController.findAllCourses()).willReturn(retrievedMarks);
+        given(markService.findAllMarks()).willReturn(retrievedMarks);
 
         // when, then
         mockMvc.perform(get(MARK_BASE_URL)
@@ -47,5 +52,41 @@ class MarkControllerTest {
                 .andExpect(jsonPath("$.marks", hasSize(2)))
                 .andExpect(jsonPath("$.marks[0].mark", is(mark1.getMark())))
                 .andExpect(jsonPath("$.marks[1].mark", is(mark2.getMark())));
+    }
+
+
+    @Test
+    void findMarkByIdTest() throws Exception {
+        // given
+        MarkDTO mark1 = new MarkDTO(5.5, LocalDateTime.now().toString(), "Ivan Doe", "History", "api/v1/mark/1");
+
+        given(markService.findMarkById(anyLong())).willReturn(mark1);
+
+        // when, then
+        mockMvc.perform(get(MARK_BASE_URL + "/13")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.mark", is(mark1.getMark())))
+                .andExpect(jsonPath("$.student_name", is(mark1.getStudentName())))
+                .andExpect(jsonPath("$.course_name", is(mark1.getCourseName())));
+
+        then(markService).should().findMarkById(anyLong());
+    }
+
+    @Test
+    void saveMarkTest() throws Exception {
+        // given
+        MarkDTO providedMark = new MarkDTO(5.5, LocalDateTime.now().toString(), "Ivan Doe", "History", "api/v1/mark/1");
+        given(markService.saveMark(any(MarkDTO.class))).willReturn(providedMark);
+        // when, then
+
+        mockMvc.perform(post(MARK_BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(providedMark)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.mark",is(providedMark.getMark())))
+                .andExpect(jsonPath("$.student_name",is(providedMark.getStudentName())));
+
+        then(markService).should().saveMark(any());
     }
 }
