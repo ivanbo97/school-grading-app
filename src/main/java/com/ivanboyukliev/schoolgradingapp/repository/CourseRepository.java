55b68523fd50fd3bdebb78e050a25bdfdaad5292
ivanbo97 <ivanboukliev@abv.bg>
2021-10-18 14:10:17 +0300

    Added repository annotation.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/repository/CourseRepository.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/repository/CourseRepository.java
@@ -3,9 +3,11 @@ package com.ivanboyukliev.schoolgradingapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ivanboyukliev.schoolgradingapp.domain.Course;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     Optional<Course> findCourseByName(String courseName);
