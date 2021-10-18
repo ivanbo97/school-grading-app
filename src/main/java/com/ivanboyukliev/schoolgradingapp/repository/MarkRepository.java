4b7a0ab444bce7240c3bc72a80426a5ec9e69f6e
ivanbo97 <ivanboukliev@abv.bg>
2021-10-18 14:09:58 +0300

    Added repository annotation.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/repository/MarkRepository.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/repository/MarkRepository.java
@@ -4,7 +4,9 @@ import com.ivanboyukliev.schoolgradingapp.domain.Mark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarkRepository extends JpaRepository<Mark, Long> {
 
     @Query("SELECT AVG(mark) FROM Mark WHERE student.id = :studentId AND course.id = :courseId")
