ae6f9ff4e1ea100560c656bbd13422a872bbe466
ivanbo97 <ivanboukliev@abv.bg>
2021-10-20 16:03:10 +0300

    Refactored permission strings.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/security/roles/SchoolSystemPermission.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/roles/SchoolSystemPermission.java
@@ -3,12 +3,12 @@ package com.ivanboyukliev.schoolgradingapp.security.roles;
 public enum SchoolSystemPermission {
 
     MARK_READ("mark:read"),
-    MARK_WRITE("mark-write"),
-    STUDENT_READ("student-read"),
-    STUDENT_WRITE("student-write"),
-    COURSE_READ("course-read"),
-    COURSE_WRITE("course-write"),
-    AVERAGE_MARK_READ("avg-read");
+    MARK_WRITE("mark:write"),
+    STUDENT_READ("student:read"),
+    STUDENT_WRITE("student:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write"),
+    AVERAGE_MARK_READ("avg:read");
 
     private final String permission;
 
