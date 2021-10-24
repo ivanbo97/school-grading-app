4b90f1033afadd280c0ff4bb408d12aa1fb20d4a
ivanbo97 <ivanboukliev@abv.bg>
2021-10-24 15:15:44 +0300

    Disabling security on api-doc endpoint.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/security/config/SecurityConfig.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/h2-console/**");
+        web.ignoring().antMatchers("/v2/api-docs");
     }
 
     @Override
