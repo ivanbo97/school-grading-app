6d332f414e5f09865724a289ded94580b322ce56
ivanbo97 <ivanboukliev@abv.bg>
2021-10-25 14:29:40 +0300

    Custom qualifier annotation for injecting bean containing number for expiration days.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/filters/jwt/TokenExpirationAfterDays.java
@@ -0,0 +1,14 @@
+package com.ivanboyukliev.schoolgradingapp.security.filters.jwt;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface TokenExpirationAfterDays {
+}
