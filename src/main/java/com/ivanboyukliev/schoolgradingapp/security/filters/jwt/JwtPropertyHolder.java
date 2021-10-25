e4f474d5b96b0d17cc608441779b1743d5b419e6
ivanbo97 <ivanboukliev@abv.bg>
2021-10-25 14:31:39 +0300

    Class for storing JWT related data.

---
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/filters/jwt/JwtPropertyHolder.java
@@ -0,0 +1,25 @@
+package com.ivanboyukliev.schoolgradingapp.security.filters.jwt;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Getter
+@Component
+public class JwtPropertyHolder {
+
+    private final String tokenPrefix;
+    private final Long expirationDays;
+    private final SecretKey secretKey;
+
+    @Autowired
+    public JwtPropertyHolder(@TokenPrefix String tokenPrefix,
+                             @TokenExpirationAfterDays Long expirationDays,
+                             SecretKey secretKey) {
+        this.tokenPrefix = tokenPrefix;
+        this.expirationDays = expirationDays;
+        this.secretKey = secretKey;
+    }
+}
