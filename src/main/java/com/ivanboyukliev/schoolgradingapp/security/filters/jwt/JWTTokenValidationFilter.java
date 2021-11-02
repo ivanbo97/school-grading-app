d2eea31ca9cd48e4e2022196fca41d5893c48843
ivanbo97 <ivanboukliev@abv.bg>
2021-11-02 09:44:35 +0200

    Implementing logic for validation of received JWT.

--- src/main/java/com/ivanboyukliev/schoolgradingapp/security/filters/jwt/JWTTokenValidationFilter.java
+++ src/main/java/com/ivanboyukliev/schoolgradingapp/security/filters/jwt/JWTTokenValidationFilter.java
@@ -1,49 +1,74 @@
 package com.ivanboyukliev.schoolgradingapp.security.filters.jwt;
 
+import com.google.common.base.Strings;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.security.authentication.BadCredentialsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.crypto.SecretKey;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import static com.ivanboyukliev.schoolgradingapp.security.filters.jwt.JWTTokenGeneratorFilter.JWT_HEADER;
-import static com.ivanboyukliev.schoolgradingapp.security.filters.jwt.JWTTokenGeneratorFilter.JWT_KEY;
-
+@Slf4j
 public class JWTTokenValidationFilter extends OncePerRequestFilter {
+
+    private final JwtPropertyHolder jwtPropertyHolder;
+
+    public JWTTokenValidationFilter(JwtPropertyHolder jwtPropertyHolder) {
+        this.jwtPropertyHolder = jwtPropertyHolder;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String receivedJWT = request.getHeader(JWT_HEADER);
 
-        if (receivedJWT != null) {
-            try {
-                SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8));
+        String authorizationHeader = request.getHeader(jwtPropertyHolder.getAuthorizationHeader());
 
-                Claims claims = Jwts.parserBuilder()
-                        .setSigningKey(key)
-                        .build()
-                        .parseClaimsJws(receivedJWT)
-                        .getBody();
+        log.info("Request token = {}", authorizationHeader);
 
-                String username = String.valueOf(claims.get("username"));
-                String authorities = String.valueOf(claims.get("authorities"));
-                Authentication auth = new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            } catch (Exception e) {
-                throw   new BadCredentialsException("Invalid token received");
-            }
+        if (Strings.isNullOrEmpty(authorizationHeader) ||
+                !authorizationHeader.startsWith(jwtPropertyHolder.getTokenPrefix())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.replace(jwtPropertyHolder.getTokenPrefix(), "");
+
+        try {
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(jwtPropertyHolder.getSecretKey())
+                    .parseClaimsJws(token);
+
+            Claims body = claimsJws.getBody();
+            String username = body.getSubject();
+
+            var authorities = (List<Map<String, String>>) body.get("authorities");
+
+            Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+                    .map(m -> new SimpleGrantedAuthority(m.get("authority")))
+                    .collect(Collectors.toSet());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    simpleGrantedAuthorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (JwtException e) {
+            throw new IllegalStateException(String.format("Token %s cannot be trusted", token));
         }
         filterChain.doFilter(request, response);
     }
